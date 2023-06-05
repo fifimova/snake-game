eb117abb04b451a06b2f166f64fa85d5adaccf7f
sasha <fi_fimova@mail.ru>
2023-06-05 12:55:47 +0300

    its game, what should i say else

---
+++ src/MainWindow.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 345);
+        setLocation(400, 400);
+        setVisible(true);
+        add(new GameField());
+    }
+
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+    }
+}
